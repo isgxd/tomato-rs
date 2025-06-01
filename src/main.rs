5719408a43372de076d0c75c5fda5c0997a03d57
g <togxd@outlook.com>
2025-06-01 18:13:45 +0800

    feat(ui): 添加时钟图标并优化窗口设置
    - 添加了时钟图标的加载和设置功能，使用 `iced::window::icon::from_file` 方法从 `assets/clock.ico` 文件中加载图标。
    - 重构了窗口设置部分，将原有的简单设置方式改为使用 `iced::window::Settings` 结构体，以便更灵活地配置窗口属性。
    - 修改了窗口大小，从 `[230.0, 50.0]` 调整为 `[230.0, 45.0]`。

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ mod app;
 mod custom_styles;
 
 use app::App;
+use iced::window;
 // use iced::Font;
 
 fn main() {
+    let icon = window::icon::from_file("assets/clock.ico").unwrap();
     iced::application(App::default(), App::update, App::view)
         .subscription(App::subscriptions)
         .theme(App::theme)
-        .decorations(false)
-        .window_size([230.0, 50.0])
+        .window(window::Settings {
+            icon: Some(icon),
+            decorations: false,
+            size: [230.0, 45.0].into(),
+            ..Default::default()
+        })
         // .font(include_bytes!("C:/Windows/Fonts/simsun.ttc"))
         // .default_font(Font::with_name("新宋体"))
         .run()
